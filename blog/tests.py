a53f7d4967243dafd4aa16da4664ac0d30d998ac
Matheus Wells <36707321+Matheuswells@users.noreply.github.com>
2021-04-19 00:56:22 -0300

    Tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+from blog.models import User, Post
+
+class  UserTestCase(TestCase):
+    newUser: User 
+    def setUp(self):
+        newUser = User.objects.create(username="userTest", name="User", last_name="Resu", birthday="1999-12-10")
+
+    def testUsers(self):
+        userTest = User.objects.get(username="userTest")
+        self.assertEqual(userTest.username,"userTest")
